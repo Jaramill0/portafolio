5a36e45de170d8585a3cb4bc8de9a9f0089efa6a
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-14 01:20:24 -0600

    Better in the picture that profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
                     <img
                         src={image}
                         alt="my profile"
-                        className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                        className='rounded-2xl mx-auto w-2/3 md:h-full md:w-full' />
                 </div>
             </div>
         </div>
