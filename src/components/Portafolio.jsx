e046babb0771803400f0a918f3b72e3ce4850beb
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-14 01:36:45 -0600

    Se agregaron los titulos de los proyectos

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -10,27 +10,33 @@ const Portafolio = () => {
     const portafolio = [
         {
             id: 1,
-            src: bot
+            src: bot,
+            title: 'BotGame'
         },
         {
             id: 2,
-            src: node
+            src: node,
+            title: 'App  Votes'
         },
         {
             id: 3,
-            src: snake
+            src: snake,
+            title:'GameSnake'
         },
         {
             id: 4,
-            src: rickymorty
+            src: rickymorty,
+            title:'Web Api Rick & Morty'
         },
         {
             id: 5,
-            src: weather
+            src: weather,
+            title: 'API Rest Weather'
         },
         {
             id: 6,
-            src: car
+            src: car,
+            title: 'E-Commerce Movies'
         }
 
     ];
@@ -41,10 +47,11 @@ const Portafolio = () => {
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portafolio</p>
                     <p className='py-6'>Echale un vistazo a algunos de mis trabajos</p>
                 </div>
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    {portafolio.map(({ id, src }) => (
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-11 sm:px-0'>
+                    {portafolio.map(({ id, src, title }) => (
                         <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                             <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
+                            <p className='mt-3 text-center font-semibold text-white-400 '>{title}</p>
                             <div className='flex items-center justify-center'>
                                 <button className='w1/3 px-7 py-2 m-4 duration-200 hover:scale-105'>Demo</button>
                                 <button className='w-1/3 px-7 py-2 m-4 duration-200 hover:scale-105'>Code</button>
