f81d2fd931e991a2493ae3100111c7cf012c25e7
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-14 00:21:28 -0600

    Actualizacion Completa de Portafolio WebSide Young

---
+++ src/components/Experience.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import javascript from '../assets/portafolio/javascript.png'
+import laravel from '../assets/portafolio/laravel.png'
+import react from '../assets/portafolio/react.png'
+import git from '../assets/portafolio/git.png'
+import github from '../assets/portafolio/github.png'
+import html from '../assets/portafolio/html.png'
+import netcore from '../assets/portafolio/netcore.png'
+import php from '../assets/portafolio/php.png'
+import tailwind from '../assets/portafolio/tailwind.png'
+
+function Experience() {
+    const tecnologias = [
+        {
+            id: 1,
+            src: javascript,
+            title: 'JavaScript',
+            style: 'shadow-yellow-400'
+        },
+        {
+            id: 2,
+            src: html,
+            title: 'CSS-HTML',
+            style: 'shadow-orange-400'
+        },
+        {
+            id: 3,
+            src: github,
+            title: 'Github',
+            style: 'shadow-gray-400'
+        },
+        {
+            id: 4,
+            src: laravel,
+            title: 'Laravel',
+            style: 'shadow-red-600'
+        },
+        {
+            id: 5,
+            src: react,
+            title: 'React',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 6,
+            src: git,
+            title: 'Git',
+            style: 'shadow-orange-500'
+        },
+        {
+            id: 7,
+            src: netcore,
+            title: '.NET-CORE',
+            style: 'shadow-fuchsia-600'
+        },
+        {
+            id: 8,
+            src: tailwind,
+            title: 'TailwindCSS',
+            style: 'shadow-sky-400'
+        },
+        {
+            id: 9,
+            src: php,
+            title: 'PHP',
+            style: 'shadow-violet-700'
+        },
+    ]
+    return (
+        <div name='experiencia' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+            <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+                <div>
+                    <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experiencia</p>
+                    <p className='py-6'>estas son las tecnologías con las que he trabajado</p>
+                </div>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                    {tecnologias.map(({ id, src, title, style }) => (
+                        <div key={id}
+                            className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`} >
+                            <img src={src} alt="" className='w-20 mx-auto' />
+                            <p className='mt-4'>{title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Experience
---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import image from '../assets/foto.jpg'
+import { MdKeyboardArrowRight } from 'react-icons/md'
+import {Link} from 'react-scroll'
+
+const Home = () => {
+    return (
+        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>Desarrollador Web Front End</h2>
+                    <p className='text-gray-500 py-4 max-w-md'>
+                        Tengo 1 año de experiencia en el desarrollo de sistemas web,
+                        Plataformas de escritorio y creación de Bots para distintos aplicativos,
+                        Me encuentro abierto a nuevas oportunidades de trabajo
+                        Enfoco mis habilidades al desarrollo web en Front End
+                        Ocupo las tecnologías:
+                        React, Angular,  Tailwind, nextjs y Laravel.
+                    </p>
+                    <div>
+                        <Link to='portafolio' smooth  duration={600} className='group text-white w-fit px-6 py-3 my-2 flex items-center 
+                    rounded-md bg-gradient-to-r from-cyan-400 to-blue-500 cursor-pointer'>
+                            Portafolio
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <MdKeyboardArrowRight size={25} className="ml-1" />
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+                <div>
+                    <img
+                        src={image}
+                        alt="my profile"
+                        className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
