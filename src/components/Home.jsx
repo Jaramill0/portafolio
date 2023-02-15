760f64d5fbd92063e73f8fddbd8a48e964d640fd
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-14 20:56:22 -0600

    change: se realizo una mejora en la foto del perfil

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import image from '../assets/foto.jpg'
+import image from '../assets/profile.jpg'
 import { MdKeyboardArrowRight } from 'react-icons/md'
 import {Link} from 'react-scroll'
 
 const Home = () => {
     return (
         <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl sm:text-7xl font-bold text-white'>Desarrollador Web Front End</h2>
                     <p className='text-gray-500 py-4 max-w-md'>
@@ -32,7 +31,8 @@ const Home = () => {
                     <img
                         src={image}
                         alt="my profile"
-                        className='rounded-2xl mx-auto w-2/3 md:h-full md:w-full' />
+                        className='rounded-2xl mx-auto w-2/3 md:w-full'
+                        sizes="(max-width: 300px) 300px, (max-width: 768px) 768px, 1280px" />
                 </div>
             </div>
         </div>
