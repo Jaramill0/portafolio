2744869a1b4dc29e32ab88712577383e82ac4573
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-14 20:42:13 -0600

    add styles in icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -49,7 +49,7 @@ const SocialLinks = () => {
         <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
             <ul>
                 {links.map(({ id, child, href, download }) =>
-                    <li key={id} className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
+                    <li key={id} className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-600">
                         <a href={href} className="flex justify-between items-center w-full text-white" target="_blank" rel="noreferrer" download={download}>
                             {child}
                         </a>
