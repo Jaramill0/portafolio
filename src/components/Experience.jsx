1ab485d2a345ae1e9215b9efc404e3a90b0b94e0
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-15 18:52:38 -0600

    Change: nombre de la tecnologia Netcore

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -50,7 +50,7 @@ function Experience() {
         {
             id: 7,
             src: netcore,
-            title: '.NET-CORE',
+            title: '.NET CORE',
             style: 'shadow-fuchsia-600'
         },
         {
@@ -70,8 +70,8 @@ function Experience() {
         <div name='experiencia' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
                 <div>
-                    <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experiencia</p>
-                    <p className='py-6'>estas son las tecnologías con las que he trabajado</p>
+                    <p className='text-4xl font-bold border-b-4  border-gray-500 p-2 inline-flex'>Experiencia</p>
+                    <p className='py-5'>estas son las tecnologías con las que he trabajado</p>
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
                     {tecnologias.map(({ id, src, title, style }) => (
