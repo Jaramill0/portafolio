e2cc23360f0a6c381463ef435dd2c4b5bdf74afe
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-15 20:42:42 -0600

    Change: mejora en el tamño de la letra

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     return (
         <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
             <div>
-                <h1 className="text-5xl font-signature ml-2">Jaramillo</h1>
+                <h1 className="text-4xl font-signature ml-3">Jaramillo</h1>
             </div>
             <ul className="hidden md:flex">
                 {links.map(({ id, link }) => (
